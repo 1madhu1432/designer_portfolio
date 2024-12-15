de73e222dde532f91e2b07e661c2ce9987a5e410
1madhu1432 <166062818+1madhu1432@users.noreply.github.com>
2024-12-15 22:37:05 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className="bg-gray-100 py-6">
             <hr className="border-t border-gray-300 mb-4" />
             <div className="text-center text-gray-500 text-sm">
-                &copy; {new Date().getFullYear()} Aakriti Saraf Design
+                &copy; {new Date().getFullYear()}  Saraf Design
             </div>
         </footer>
     );
